816065d9bfc9277cb75bee17812b43a88461cd1d
susmitha <susmithareddychinthala024@gmail.com>
2022-11-01 12:31:22 +0530

    Added Welcome message for Generic Problems

---
+++ src/com/generics/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.generics;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("-------@@@@@@----WELCOME TO GENERIC PROBLEMS----@@@@@@@-------");
+    }
+}
