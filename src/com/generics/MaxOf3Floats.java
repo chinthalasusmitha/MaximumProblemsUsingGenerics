086e42377761ec90543c03fe13cb991f19a17ed2
susmitha <susmithareddychinthala024@gmail.com>
2022-11-01 13:50:50 +0530

    Added: Refactored the code to get Maximum from 3 Float values

--- src/com/generics/MaxOf3Floats.java
+++ src/com/generics/MaxOf3Floats.java
@@ -1,20 +1,24 @@
 package com.generics;
-//Use Case 1 is to give 3 integers and find out the maximum and its position in array.
 
 import java.util.Scanner;
 
-public class FindMaximum {
-        public static void main(String[] args) {
-            FindMaximum testMaximum = new FindMaximum();
+//Use Case 1 is to give 3 integers and find out the maximum and its position in array.
+//Use Case 2 is to give 3 floats and find out the maximum and its position in array.
+public class MaxOf3Floats {
 
-            Integer[] array = addToArray();            //Define the array where size is 3
+
+        public static void main(String[] args) {
+            MaxOf3Floats testMaximum = new MaxOf3Floats();
+
+            Float[] array = addToArray();            //Define the array where size is 3
+
             printArray(array);
             getMaximum(array);
         }
 
-        //Get maximum from the array and its position.
-        public static void getMaximum(Integer[] array) {
-            int max = array[0];
+        //Get maximum from the array and its position
+        public static void getMaximum(Float[] array) {
+            float max = array[0];
             int position = 0;
             for (int i = 0; i < array.length; i++)
             {
@@ -29,8 +33,8 @@ public class FindMaximum {
             System.out.println("Maximum integer position is : "+position+" in array.");
         }
 
-        //Printing to array.
-        public static void printArray(Integer[] array) {
+        //Printing the array.
+        public static void printArray(Float[] array) {
             System.out.println("Array is : ");
             for (int i = 0; i < array.length; i++)
             {
@@ -39,13 +43,14 @@ public class FindMaximum {
         }
 
         //Adding values to array.
-        public static Integer[] addToArray() {
+        public static Float[] addToArray() {
             System.out.println("Enter 3 values");
             Scanner scan = new Scanner(System.in);
-            int x = scan.nextInt();
-            int y = scan.nextInt();
-            int z = scan.nextInt();
+            float x = scan.nextFloat();
+            float y = scan.nextFloat();
+            float z = scan.nextFloat();
             scan.close();
-            return new Integer[]{x,y,z};
+            return new Float[]{x,y,z};
         }
     }
+
